b754455fcfc3a6cb159e27aa8172dfaa85fdf33b
Parth Mehta <parth@id8labs.com>
2021-09-14 15:35:29 +0530

    + Recycler view and added Databinding with API call

---
+++ app/src/main/java/com/sample/recyclerviewwithdatabinding/RetroInstance.kt
@@ -0,0 +1,20 @@
+package com.sample.recyclerviewwithdatabinding
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetroInstance {
+
+    companion object {
+
+        val BASE_URL = "https://api.github.com/search/"
+
+        fun getRetroInstance(): Retrofit {
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
+}
